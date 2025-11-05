ccf113ce5e9facd0d2079a615faa5ea943314380
bealals <icampa74@pimajted.net>
2025-11-05 12:27:25 -0700

    Refactor GameRunner initialization and remove unused code
    Refactor GameRunner to initialize Game with current room and remove unused greeting and input handling.

--- GameRunner.java
+++ GameRunner.java
@@ -1,25 +1,12 @@
 import java.util.Scanner;
+
 public class GameRunner{
     public static void main(String[] args){
-        Game womp = new Game();  // creates a Magpie2 object named maggie		
-        String greeting = womp.getGreeting();  // calls the getGreeting method 
-                                               //     on the maggie object
-        System.out.println(greeting);
+        Scanner scan = new Scanner(system.in);
+        int currRoom = 1;
 
-        Scanner in = new Scanner (System.in);   // creates a Scanner object
-                                                //   to read from the keyboard
-        
-        String statement = in.nextLine();       // reads a line of text 
-                                                //   from the keyboard
-            
-        while (!statement.equals("Bye"))       // loop stopping condition
-        {                  // calls the getResponse method on the maggie object
-            String response = womp.getResponse(statement);                    
-            System.out.println(response);         
-            statement = in.nextLine();        
-        }
-        System.out.println("Have a nice day, I guess.");
-        in.close();
+        Game game = new Game(currRoom);
+
     }
 }
 
@@ -34,8 +21,21 @@ public class Game{
             String response = "Great choice, " + name + "!";
             System.out.println(response);
         }
+        private String roomOne(){
+
+        }
+        private String roomTwo(){
+
+        }
+        private String roomThree(){
+
+        }
+        private String roomFour(){
+
+        }
 
 
     }
         
 }
+

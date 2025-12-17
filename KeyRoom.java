f00cc288d02a2de4ed5a28a65051c51c7400b0ea
bealals <icampa74@pimajted.net>
2025-12-17 10:13:27 -0700

    Enhance KeyRoom class with descriptive comments
    Added comments to clarify class functionality.

--- KeyRoom.java
+++ KeyRoom.java
@@ -1,5 +1,7 @@
 public class KeyRoom {
-
+// This class is fairly simple, since it only is meant to be able to unlock a door, which also happens to be the most that some of the rooms are meant to do
+// The main purpose of this class is mainly to create a break area where all the player is needed to do is simply insert and turn a key
+    // sets as default
     private boolean inserted = false;
 
     public boolean attempt(String input){
@@ -10,12 +12,13 @@ public class KeyRoom {
             return false;
         }
         if (input.equals("turn key") && inserted){
-            System.out.println("Door unlocked.");
+            System.out.println("Door unlocked."); // this is what you want
             return true;
         }
-        System.out.println("Nothing happens.");
+        System.out.println("Nothing happens."); // this is what you don't want
         return false;
     }
 }
 
     
+
